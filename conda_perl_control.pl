e56d5a19c0adea5cce376bd3320cdca294e5b65e
raddysr <raddy.radev@gmail.com>
2020-07-03 23:34:12 +0300

    conada run/stop

---
+++ conda_perl_control.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+
+my $param = shift;  
+if(!$param){
+	print"Run wiht parameter -s/-r!\n"; 
+	exit; 
+}
+
+
+
+if($param eq '-s'){
+	qx/conda config --set auto_activate_base false/;
+	print"Anaconda stopped!\n"; 
+}elsif($param eq '-r'){
+	qx/conda config --set auto_activate_base true/;
+	print"Anaconda is now running!\n"; 
+}
+
+
