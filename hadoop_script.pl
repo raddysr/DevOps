63c8f0468a970fed880b69b7a0f1e68b83798b77
raddysr <raddy.radev@gmail.com>
2020-10-24 11:53:26 +0300

    stop/start

---
+++ hadoop_script.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $signal	= shift;
+my $start	= "/home/hadoop/hadoop-3.3.0/sbin/start-dfs.sh";
+my $start_yarn	= "/home/hadoop/hadoop-3.3.0/sbin/start-yarn.sh";
+my $stop 	= "/home/hadoop/hadoop-3.3.0/sbin/stop-dfs.sh";
+my $stop_yarn	= "/home/hadoop/hadoop-3.3.0/sbin/stop-yarn.sh";
+
+if( $signal eq 'start'){
+
+	print qx/$start/;	
+	print qx/$start_yarn/;
+
+}elsif( $signal eq 'stop'){
+	print qx/$stop/;
+	print qx/$stop_yarn/; 
+}else{
+	print"Run script with start or stop argumet";
+}
+
