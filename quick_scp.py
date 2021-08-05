7f6f3ea025e4d925ce6a504e51d64f44012b0880
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-08-05 11:23:18 +0300

    Create quick_scp.py

---
+++ quick_scp.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import paramiko
+from scp import SCPClient
+from sys import argv
+from os import system
+
+HOST='lxaviap001'
+PORT = 22
+USER='aaaiviz'
+PASS = argv[1]
+SOURCE_PATH = '/home/postgres_bak/'
+TARGET_PATH = '/home/aaaiviz/postgres_backup/'
+BACKFILES = argv[2:]
+
+def scp_to_backup(host, port, user, password, source, target):
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        ssh_client.connect(host, port , user, password)
+        scpclient = SCPClient(ssh_client.get_transport(),socket_timeout=15.0)
+
+        try:
+            scpclient.put(source, target)
+        except FileNotFoundError as e:
+            print(e)
+            print ("The system cannot find the file specified" + source)
+        else:
+                 print ("File uploaded successfully")
+        ssh_client.close()
+
+
+
+
+for b in BACKFILES:
+        source = f'{SOURCE_PATH}{b}'
+        target = f'{TARGET_PATH}{b}'
+        scp_to_backup(HOST, PORT, USER, PASS, source, target)
+
+
+system('rm -f /home/postgres_bak/*.dump')
